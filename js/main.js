7ae2eb00379c84d8e2fd96e9a669c48caeca159a
Ben Coullie <bencoullie@gmail.com>
2016-01-29 17:31:27 +0900

    added desktop box basic layout, finished right side (+html5 validation)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(function(){
+  $(".js-phone-number").hover(function(){
+    $(".inner-header .phone-number div").css({"display": "inline-block"});
+  }, function(){
+    $(".inner-header .phone-number div").css({"display": "none"});
+  });
+});
