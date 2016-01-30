f9882651b2744a14d772a1edd870b5b2592e9577
Ben Coullie <bencoullie@gmail.com>
2016-01-30 11:39:56 +0900

    successfully added unslider to desktop, desktop version finished and pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 "use strict"; // for ECMA6
 
-// Simple header phone icon functionality
+
 $(function(){
+  // Simple header phone icon functionality
   $(".js-phone-number").hover(
     () => $(".inner-header .phone-number div").css({"display": "inline-block"}),
     () => $(".inner-header .phone-number div").css({"display": "none"})
   );
+
+  // unSlider activation
+  $('.my-slider').unslider();
 });
 
 // Just FYI If you wanted to see how I would do a plain old javascript validation
 // of just the email field I'd probably just use a regex test like this:
 
 // function validateEmail(email) {
-//   var re = [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?;
-//   return re.test(email);
+//   let re = [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?;
+//   let emailTest = re.test(email input);
+//   I'd then block/accept form submission and do something visual with this boolean.
 // }
 
 // Source of regex here (I wish I wrote this...): http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
