ade6065492e796750670c0971581c3f32c2a2b16
Ben Coullie <bencoullie@gmail.com>
2016-01-30 09:23:05 +0900

    added some comments. Pre-carousel commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+"use strict"; // for ECMA6
+
+// Simple header phone icon functionality
 $(function(){
-  $(".js-phone-number").hover(function(){
-    $(".inner-header .phone-number div").css({"display": "inline-block"});
-  }, function(){
-    $(".inner-header .phone-number div").css({"display": "none"});
-  });
+  $(".js-phone-number").hover(
+    () => $(".inner-header .phone-number div").css({"display": "inline-block"}),
+    () => $(".inner-header .phone-number div").css({"display": "none"})
+  );
 });
+
+// Just FYI If you wanted to see how I would do a plain old javascript validation
+// of just the email field I'd probably just use a regex test like this:
+
+// function validateEmail(email) {
+//   var re = [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?;
+//   return re.test(email);
+// }
+
+// Source of regex here (I wish I wrote this...): http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
